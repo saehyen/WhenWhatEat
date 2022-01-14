db73c0bc8a47c3cd73d66338eb6ccde13499e552
KRdoncc <cow1730@naver.com>
2022-01-14 20:46:46 +0900

    Backend -> Recipe.vue 데이터 통신 1차 완료.

--- back/src/main/java/com/hustar/recipe/controller/RecipeController.java
+++ back/src/main/java/com/hustar/recipe/controller/RecipeController.java
@@ -1,10 +1,13 @@
 package com.hustar.recipe.controller;
 
+import java.io.IOException;
 
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,30 +18,29 @@ import com.hustar.recipe.vo.ResultVO;
 @Controller
 @RequestMapping("recipe/")
 public class RecipeController {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(RecipeController.class);
-	
+
 	@Autowired
 	RecipeService recipeService;
-	
+
+	@CrossOrigin(origins = "*")
 	@ResponseBody
-	@RequestMapping(value = "get-recipe-list.do", method = RequestMethod.GET)
-	public ResultVO getRecipeList()
-	{
-		
+	@RequestMapping(value = "get-recipe-list.do", method = RequestMethod.POST)
+	public ResultVO getRecipeList() throws IOException, ParseException {
+
 		LOG.info("[GET] getRecipeList");
-		
+
 		ResultVO result = new ResultVO(false, null);
-		
+
 		try {
 			result.setResult(recipeService.getRecipeList());
 			result.setSuccess(true);
 		} catch (Exception e) {
 			LOG.error("[Recipe] getRecipeList : " + e.getMessage(), e);
 		}
-		
+
 		return result;
 	}
-	
 
 }
