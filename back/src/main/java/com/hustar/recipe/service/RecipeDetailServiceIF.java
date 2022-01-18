dea720abf21c44f3865578ecc58b50bca1cb37d0
KRdoncc <cow1730@naver.com>
2022-01-18 15:18:52 +0900

    backend RecipeDetail 통신.

--- back/src/main/java/com/hustar/recipe/service/RecipeDetailServiceIF.java
+++
@@ -1,9 +0,0 @@
-package com.hustar.recipe.service;
-
-import java.util.List;
-
-import com.hustar.recipe.vo.RecipeDetailVO;
-
-public interface RecipeDetailServiceIF {
-	public List<RecipeDetailVO> getRecipeDetailList();
-}
--- back/src/main/java/com/hustar/recipe/service/RecipeService.java
+++ back/src/main/java/com/hustar/recipe/service/RecipeService.java
@@ -2,6 +2,7 @@ package com.hustar.recipe.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,27 +10,66 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hustar.recipe.mapper.RecipeMapper;
+import com.hustar.recipe.vo.RecipeDetailVO;
 import com.hustar.recipe.vo.RecipeVO;
 
 @Service
-public class RecipeService implements RecipeServiceIF{
-	
+public class RecipeService implements RecipeServiceIF {
+
 	@Autowired
 	private SqlSessionFactory sqlSessionFactory;
+
+
+
 
 	@Override
 	public List<RecipeVO> getRecipeList() {
 
 		List<RecipeVO> recipeList = new ArrayList<RecipeVO>();
-		
+
 		try (SqlSession session = sqlSessionFactory.openSession()) {
 			RecipeMapper mapper = session.getMapper(RecipeMapper.class);
-			
-				recipeList = mapper.getRecipeList();
+
+			recipeList = mapper.getRecipeList();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return recipeList;
 	}
+	
+	public RecipeVO getRecipeDetailList(Long id) {
+		RecipeVO recipe = new RecipeVO();
+		
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			RecipeMapper mapper = session.getMapper(RecipeMapper.class);
+ 
+			recipe = mapper.getRecipeDetailList(id);
+ 
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return recipe;
+	}
+	
+	public List<RecipeDetailVO> getRecipeDetail(Long id) {
+//		RecipeDetailVO recipeDetail = new RecipeDetailVO();
+		List<RecipeDetailVO> recipeDetail = new ArrayList<RecipeDetailVO>();
+		
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			RecipeMapper mapper = session.getMapper(RecipeMapper.class);
+ 
+			recipeDetail = mapper.getRecipeDetail(id);
+ 
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return recipeDetail;
+	}
+	
+	
+
+
 
 }
