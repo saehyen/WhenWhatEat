109f041d6f55aeb5a66b4481d37bb40871bb9b8f
saehyen <79187118+saehyen@users.noreply.github.com>
2022-01-23 17:59:34 +0900

    Update board.vue

--- frontend/src/views/board.vue
+++ frontend/src/views/board.vue
@@ -2,44 +2,7 @@
 
 <div>
   <v-container>
-    <table class="table">
-      <thead>
-        <tr> 
-          <th scope="col" style="width:20%;">게시글 번호</th> 
-          <th scope="col" style="width:40%;">제목</th>
-          <th scope="col" style="width:20%;">작성일</th>
-          <th scope="col" style="width:20%;">조회수</th>     
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="data in data.content" :key="data.noticeIdx" id="board_tr">
-          <td scope="row">{{data.noticeIdx}}</td>
-          <td><a v-on:click="view(data)" id="board_title">{{data.title}}</a></td>
-          <td scope="row">{{data.registDate}}</td>
-          <td scope="row">{{data.views}}</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <v-row d-flex align="center"
-      justify="center">
-      <v-col align="center"
-      justify="center">
-        <router-link 
-        class="button" 
-        to="/create"
-        >작성하기</router-link>
-      </v-col>
-    </v-row>
-    <v-row>
-      <div class="">
-          <v-pagination 
-            v-model="data.currentPage"
-            :length="data.totalPages"
-            @input="handlePageChange"
-          ></v-pagination>    
-      </div>
-    </v-row>
+    
   </v-container>
   </div>
 </template>
@@ -163,80 +126,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-
-}
-.pagination-wrapper{
-   display:flex;
-}
-.pageBtn{
-   display: flex;
-    align-content: stretch;
-    padding: 10px;
-    justify-content: center;
-}
-.table {
-  width:100%;
-  text-align: center;
-  color:white;
-  font-size:1.3rem;
-}
-.table a{
-  font-size:1.3rem;
-}
-table tbody tr{
-  height:35px;
-}
-#board_tr{
-  height:80px;
-  vertical-align: middle;
-}
-#board_title{
-  cursor:pointer;
-}
-/* button 스타일링 */
-a {
-   -webkit-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
-   -moz-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
-   -ms-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
-   -o-transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
-   transition: all 200ms cubic-bezier(0.390, 0.500, 0.150, 1.360);
-
-   margin: 0 auto;
-   max-width: 100px;
-  height:40px;
-  line-height:30px;
-   text-decoration: none;
-   border-radius: 4px;
-   padding: 5px;
-  font-size:14px;
-  font-weight:600;
-  text-align: center;
-}
-
-a.button {
-   color: rgba(30, 22, 54, 0.6);
-   box-shadow: rgba(30, 22, 54, 0.4) 0 0px 0px 2px inset;
-  background-color:white;
-}
-
-a.button:hover {
-   color: rgba(255, 255, 255, 0.85);
-   box-shadow: rgba(30, 22, 54, 0.7) 0 0px 0px 40px inset;
-}
 
 </style>
