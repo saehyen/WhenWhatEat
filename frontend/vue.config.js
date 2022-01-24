bf6bdee45acad83dcc5cc2fb893547b99d929c5e
saehyen <rlatpgus4564@naver.com>
2022-01-24 15:20:54 +0900

    cors 테스트2

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,9 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  //outputDir: '/var/www/html/' ,
   devServer: {
-    proxy : 'http://ec2-13-124-134-65.ap-northeast-2.compute.amazonaws.com:8080'
+    proxy : 'http://ec2-13-124-134-65.ap-northeast-2.compute.amazonaws.com:8080',
+    disableHostCheck: true
   }
 }
